757e8cb60a1611a668f4b1ac87fd9fc175905bef
takizawa0548 <takizawa020383461@gmail.com>
2022-03-20 10:36:53 +0900

    1st commit

---
+++ db/init/02_insert_date.sql
@@ -0,0 +1,5 @@
+INSERT INTO books VALUES(1, 'プログラミング言語C', current_timestamp);
+INSERT INTO books VALUES(2, 'やさしいコンピューター科学', current_timestamp);
+INSERT INTO books VALUES(3, 'ゲーテル、エッシャー、バッハーあるいは不思議の環', current_timestamp);
+INSERT INTO books VALUES(4, 'TeXブック コンピュータによる組版システム', current_timestamp);
+INSERT INTO books VALUES(5, 'おじいちゃん', current_timestamp);
